07752941feb2ca8b85dd7f33a5cc6b03359e2cda
snelakanti <vnelakanti@gmail.com>
2018-02-26 09:19:48 -0500

    add function that carries out all the operations and call it

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var years = [1963,1964,1988,1990,1994,2017];
-var ages = [];
-
-for(i = 0; i < years.length; i++ ){
-
-    ages[i] = 2018- years[i];
+function printFullAge(years){
+  
+    var ages = [];
+    var fullAges = [];
+    for(i = 0; i < years.length; i++ ){
+    
+        ages[i] = 2018- years[i];
+    
+        
+    }
 
     
-}
+    for (i = 0; i < ages.length; i++){
+    
+        if(ages[i] > 18){
+    
+            console.log(ages[i] + " is Adult!");
+            fullAges.push(true);
+        }
+        else{
+            console.log(ages[i] + " is Underage");
+            fullAges.push(false);
+        }
+    };
+    
+return fullAges;
+};
 
-console.log(ages);
-
-for (i = 0; i < ages.length; i++){
-
-    if(ages[i] > 18){
-
-        console.log("Adult!");
-    }
-    else{
-        console.log("Underage");
-    }
-} 
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2011,2013,1987]);
